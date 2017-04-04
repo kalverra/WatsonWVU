c1b3129e040083eaa7fe1c781638ed2e33970c90
Adam Hamrick <hamdiesel5@gmail.com>
2017-04-04 16:07:56 -0400

    Added economy classification to statements

---
+++ statement_parse.py
@@ -0,0 +1,18 @@
+import json
+import csv
+
+csv_writer = csv.writer(open("statements.csv", "w"))
+statements = json.load(open("statements.json", "r"))
+
+csv_writer.writerow(["Statement", "Ruling", "Economics (T/F)"])
+
+for statement in statements:
+    about_economy = "F"
+    for subject in statement["subject"]:
+        if subject["subject_slug"] == "economy":
+            about_economy = "T"
+    csv_writer.writerow([
+        statement["ruling_headline"].encode("ascii", "ignore"),
+        statement["ruling"]["ruling"].encode("ascii", "ignore"),
+        about_economy
+    ])

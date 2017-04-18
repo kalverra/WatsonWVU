5a6e8cfc83b314a3d976f8de80c504097a128907
Nick Miles <nickdotmiles@gmail.com>
2017-04-18 16:12:24 -0400

    updated web search
    soooo if we want to go a step further than just getting keyword based on
    a whole URL, this will pull out the sentences in the source that contain
    at least two of the keywords, then we could compare just most likely
    sentences in a source to be relevant to fact checking.

--- WebSearch.py
+++ WebSearch.py
@@ -6,6 +6,9 @@ from googleapiclient.discovery import build
 #import json
 #google seach API key: AIzaSyDIwiJp6e4KTXbU_qA9ZDnvBckCEBMTDlo
 
+def main():
+	webSearch("Donald Trump")
+
 def webSearch(keyWordString):
 	numOfResults = 10
 	query = keyWordString
@@ -15,26 +18,47 @@ def webSearch(keyWordString):
 	service = build("customsearch","v1",developerKey="AIzaSyDIwiJp6e4KTXbU_qA9ZDnvBckCEBMTDlo")
 
 	#q is the query, cx specifies the custom search, num is number of results
-	res = service.cse().list(q = query,cx="018123512344280340302:tqa4kiqukzs",num = numOfResults).execute()
+	#res = service.cse().list(q = query,cx="018123512344280340302:tqa4kiqukzs",num = numOfResults).execute()
 	links = []
 	count = 0
 	#parse the josn for the desired links in the goggle search results
 	while count < numOfResults:
-		links.append(res["items"][count]["link"])
+#		links.append(res["items"][count]["link"])
 		count+=1
 	count = 0
 	pprint.pprint(links)
 	
 	
-#	while count < numOfResults:
-		
-		#send link to watson to return weighted keywords to check against fact keywords
+	sentences = []
+	keywords = []
+	keywords.append("Donald")
+	keywords.append("Trump")
+	keywords.append("taxes")
+	
+	txt = "Donald is lame. he is not a good guy. I  dont like Donald Trump. Trump knows taxes. The Donald won't release his taxes. Donald Trump has paid many taxes"	#send link to watson to return String body of text.
+	
+	while count < 1 :
+		while(len(keywords)>1):
+			firstCheck = keywords.pop()
+			for secondCheck in keywords:
+				for sentence in txt.split('.'):
+					if secondCheck in sentence and firstCheck in sentence:
+						sentences.append(sentence)# adds a sentence to this list if it contains ataleast 2 or the keywords
+	
+				count+=1
+			
 
-		#compare two sets of keywords against eachother semantically
+#	for sentence in sentences:
+		#get keywords from watson
+		#compare the keywords of each sentance to the keywords of the fact we're checking
 		
 		
-		
-   
+	pprint.pprint(list(set(sentences)))
+	
+	
+	
+	
+
 #This is some code we probably can't really use, but it 
 #checks each link for an exact matching string within the page
 '''	
@@ -49,3 +73,7 @@ for link in links:
 	else:
 		print 'My string is in the html'
 '''		
+
+
+if '__main__' == __name__:
+	webSearch("Donald Trump")

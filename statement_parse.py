082fcf347de55db40077643541ec8f1a6a7e3604
Adam Hamrick <hamdiesel5@gmail.com>
2017-04-19 14:45:30 -0400

    Parsed em

--- statement_parse.py
+++ statement_parse.py
@@ -1,8 +1,8 @@
 import json
 import csv
 
-csv_writer = csv.writer(open("statements.csv", "w"))
-statements = json.load(open("statements.json", "r"))
+csv_writer = csv.writer(open("economic_statements.csv", "w"))
+statements = json.load(open("economic_statements.json", "r"))
 
 csv_writer.writerow(["Statement", "Ruling", "Economics (T/F)"])
 
@@ -12,7 +12,7 @@ for statement in statements:
         if subject["subject_slug"] == "economy":
             about_economy = "T"
     csv_writer.writerow([
-       "\""+ statement["ruling_headline"].encode("ascii", "ignore")+"\"",
+        "\"" + statement["ruling_headline"].encode("ascii", "ignore") + "\"",
         statement["ruling"]["ruling"].encode("ascii", "ignore"),
         about_economy
     ])
